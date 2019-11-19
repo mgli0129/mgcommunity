7364bfbf2d9683e135e9e2bf278e095e4c75dbdb
mg <mgli0129@vip.qq.com>
2019-11-19 17:32:13 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spring boot 学习笔记
+
+## 一、参考文档
+
+[https://spring.io/guides](https://spring.io/guides)
+
+
+## 二、学习内容
+####2019/11/19
+####@TaskToday
+1. sprint boot 环境部署；
+2. git/github 部署；
--- src/main/java/com/mg/community/controller/HelloWorldController.java
+++ src/main/java/com/mg/community/controller/HelloWorldController.java
@@ -4,15 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
 
     @GetMapping("/demo/{name}")
-    public String demo(@PathVariable String name, Model model){
+    public String demo(@PathVariable String name, Model model) {
         name = name + " ^_^";
         model.addAttribute("name", name);
         return "demo";
