4a52a8054ddc8dd68347705d94e2c6e0e0a39dd2
mg <mgli0129@vip.qq.com>
2019-11-19 15:17:10 +0800

    mg 0002

--- src/main/java/com/mg/community/controller/HelloWorldController.java
+++ src/main/java/com/mg/community/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.mg.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/demo")
-    public String demo(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model, HttpServletRequest request){
+    @GetMapping("/demo/{name}")
+    public String demo(@PathVariable String name, Model model){
         name = name + " ^_^";
         model.addAttribute("name", name);
         return "demo";
