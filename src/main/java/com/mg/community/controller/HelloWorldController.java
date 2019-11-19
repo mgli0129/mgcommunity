4d77a25847e4a7058b97fd49da431a6483b1a2d8
mg <mgli0129@vip.qq.com>
2019-11-19 17:56:32 +0800

    ...

---
+++ src/main/java/com/mg/community/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.mg.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/demo/{name}")
+    public String demo(@PathVariable String name, Model model) {
+        name = name + " ^_^";
+        model.addAttribute("name", name);
+        return "demo";
+    }
+
+}
